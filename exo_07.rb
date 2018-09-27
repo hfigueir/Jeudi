79d0980e4e79d1c3d481d9a002731cd9444ee1c4
hfigueir <42937224+hfigueir@users.noreply.github.com>
2018-09-27 16:14:38 +0200

    Add files via upload

---
+++ exo_07.rb
@@ -0,0 +1,9 @@
+user_name = gets.chomp
+puts user_name
+
+# gets demande à l'utilisateur d'entrer quelque chose
+#chomp retire le \n
+#Le premier programme demande d'entrer son nom puis affiche ce qu'a rentrer l'utilisateur
+#le 2eme programme demande d'entrer son nom puis affiche un > pour que l'utilisateur voit où il va ecrire
+#le 3eme n'affiche pas de texte, donc l'utilisateur ne sait pas ce qu'il doit ecrire et quand ecrire
+
